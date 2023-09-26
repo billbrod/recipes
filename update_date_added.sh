bd7a37c07f1cb70b07e3f876a303c0a1d088b29f
William F. Broderick <billbrod@gmail.com>
2023-09-25 23:11:05 -0400

    make update_date_added  a table

--- update_date_added.sh
+++ update_date_added.sh
@@ -3,11 +3,14 @@ set -euo pipefail
 
 sed -i '/{ #recent }/q' docs/index.md
 echo "" >> docs/index.md
+echo "|Recipe| Date added|" >> docs/index.md
+echo "|------|-----------|" >> docs/index.md
 all_files="$(git ls-files docs/recipes/*md)"
 for file in ${all_files[*]}; do
     if [[ $file != *"index.md" ]]; then
         recipe_name="$(grep -h '^# ' $file)"
         date="$(grep -h 'Date added' $file)"
-        echo "- ${recipe_name/\# /}: ${date/- Date added: /}" >> docs/index.md
+        file=${file/docs\///}
+        echo "|[${recipe_name/\# /}](${file/.md//})|${date/- Date added: /}|" >> docs/index.md
     fi
 done
