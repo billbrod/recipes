5006199fba2b950bc30bf6cee0c5b6841c2f539f
William F. Broderick <billbrod@gmail.com>
2024-09-12 20:48:34 -0400

    adds wakelock

---
+++ docs/javascripts/wakelock.js
@@ -0,0 +1,19 @@
+document$.subscribe(function() {
+    const toggle = '<label for="wakelock-toggle">Keep screen on<label class="switch"> <input id="wakelock-toggle" type="checkbox"> <span class="slider round"></span> </label></label>'
+    $("#ingredients").before(toggle)
+    let wakelock;
+    const canWakeLock = () => 'wakeLock' in navigator;
+    $('#wakelock-toggle').on("change", async function(){
+        if(!canWakeLock()) return;
+        try {
+            if (wakelock) {
+                wakelock.release();
+                wakelock = null;
+            } else {
+                wakelock = await navigator.wakeLock.request();
+            }
+        } catch(e) {
+            console.error('Failed to lock wake state with reason:', e.message);
+        }
+    })
+})
